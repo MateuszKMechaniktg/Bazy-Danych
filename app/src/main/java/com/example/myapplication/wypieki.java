a3f03cab338cf336133292f1971463240fcb520f
MateuszKMechaniktg <mateusz.kubica@uczen.mechaniktg.pl>
2026-03-17 11:33:31 +0100

    Encja danych

--- app/src/main/java/com/example/myapplication/wypieki.java
+++ app/src/main/java/com/example/myapplication/wypieki.java
@@ -1,5 +1,65 @@
 package com.example.myapplication;
 
-public class wypieki {
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
+@Entity (tableName = "wypieki_tabela")
+public class wypieki {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String nazwa;
+    private String skladniki;
+    @ColumnInfo(name = "tempertaura pieczenia")
+    private int temperatura;
+    @ColumnInfo(name = "czas pieczenia")
+    private int czas;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public String getSkladniki() {
+        return skladniki;
+    }
+
+    public void setSkladniki(String skladniki) {
+        this.skladniki = skladniki;
+    }
+
+    public int getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(int temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    public int getCzas() {
+        return czas;
+    }
+
+    public void setCzas(int czas) {
+        this.czas = czas;
+    }
+    public wypieki(String nazwa, String skladniki, int temperatura, int czas) {
+        id = 0;
+        this.nazwa = nazwa;
+        this.skladniki = skladniki;
+        this.temperatura = temperatura;
+        this.czas = czas;
+    }
 }
+
